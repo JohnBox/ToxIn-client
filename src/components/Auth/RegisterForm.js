03f9e3153fea1d1249ecb8efa84e15e6da39b98e
gott <johnboxwon@gmail.com>
2018-06-06 16:39:09 +0300

    refactor sign up page

--- src/components/Auth/RegisterForm.js
+++ src/components/Auth/RegisterForm.js
@@ -1,35 +1,35 @@
-var React = require('react');
-var { ajax } = require('jquery');
-var mui = require('material-ui');
-var { RaisedButton, TextField } = mui;
+let React = require('react');
+let { ajax } = require('jquery');
+let mui = require('material-ui');
+let { RaisedButton, TextField } = mui;
 
 module.exports = React.createClass({
   getDefaultProps() {
-    return {url: 'http://91.225.146.97:8000/'}
+    return {url: 'http://127.0.0.1:8000/'}
   },
   getInitialState() {
-    return {username: '', first_name: '',last_name: '', email: '', passwd: '', confirm_passwd: ''};
+    return {username: '', first_name: '',last_name: '', email: '', password: '', confirm_password: ''};
   },
   isValid() {
-    var f = this.state;
-    return f.username && f.first_name && f.last_name && f.email && f.passwd;
+    let f = this.state;
+    return f.username && f.first_name && f.last_name && f.email && f.password;
   },
-  isPasswdValid() {
-    return this.state.passwd === this.state.confirm_passwd;
+  isPasswordValid() {
+    return this.state.password === this.state.confirm_password;
   },
   onSubmit(e) {
-    var that = this;
+    let that = this;
     e.preventDefault();
-    if (this.isValid() && this.isPasswdValid()) {
+    if (this.isValid() && this.isPasswordValid()) {
       ajax({
-        url: this.props.url + 'register/',
+        url: this.props.url + 'sign-up/',
         method: 'POST',
         data: {
           username: this.state.username,
           first_name: this.state.first_name,
           last_name: this.state.last_name,
           email: this.state.email,
-          passwd: this.state.passwd
+          password: this.state.password
         },
         success: function (data) {
           if (!data.e) {
@@ -47,28 +47,28 @@ module.exports = React.createClass({
   usernameInput(e) {
     this.setState({username: e.target.value});
   },
-  firstnameInput(e) {
+  firstNameInput(e) {
     this.setState({first_name: e.target.value});
   },
-  lastnameInput(e) {
+  lastNameInput(e) {
     this.setState({last_name: e.target.value});
   },
   emailInput(e) {
     this.setState({email: e.target.value});
   },
   passwordInput(e) {
-    this.setState({passwd: e.target.value});
+    this.setState({password: e.target.value});
   },
   confirmPasswordInput(e) {
-    this.setState({confirm_passwd: e.target.value});
+    this.setState({confirm_password: e.target.value});
   },
   render() {
     return (
       <div className="register_form">
         <form onSubmit={this.onSubmit}>
           <TextField hintText={'Логін'} onChange={this.usernameInput}/><br/>
-          <TextField hintText={'Ім`я'} onChange={this.firstnameInput}/><br/>
-          <TextField hintText={'Прізвище'} onChange={this.lastnameInput}/><br/>
+          <TextField hintText={'Ім`я'} onChange={this.firstNameInput}/><br/>
+          <TextField hintText={'Прізвище'} onChange={this.lastNameInput}/><br/>
           <TextField type="email" hintText={'Електронна пошта'} onChange={this.emailInput}/><br/>
           <TextField type="password" hintText={'Пароль'} onChange={this.passwordInput}/><br/>
           <TextField type="password" hintText={'Підтвердження паролю'} onChange={this.confirmPasswordInput}/><br/><br/>
