e0438ccb2ed4fefd2c8aae07b89a7f2c663121f2
gott <johnboxwon@gmail.com>
2018-06-07 15:23:30 +0300

    fix user contacts

--- src/components/Tab/HomeTab.js
+++ src/components/Tab/HomeTab.js
@@ -1,12 +1,12 @@
-var React = require('react');
-var $ = require('jquery');
-var Cookie = require('js-cookie');
-var ScrollBar = require('react-scrollbar');
-var mui = require('material-ui');
-var { Paper, TextField, Menu, SubheaderMenuItem, SvgIcon, FlatButton, List, ListDivider} = mui;
-var StylePropable = mui.Mixins.StylePropable;
-var Colors = mui.Styles.Colors;
-var windowTypes = require('../windows');
+const React = require('react');
+const $ = require('jquery');
+const Cookie = require('js-cookie');
+const ScrollBar = require('react-scrollbar');
+const mui = require('material-ui');
+const { Paper, TextField, Menu, SubheaderMenuItem, SvgIcon, FlatButton, List, ListDivider } = mui;
+const StylePropable = mui.Mixins.StylePropable;
+const Colors = mui.Styles.Colors;
+const windowTypes = require('../windows');
 
 
 
@@ -15,19 +15,23 @@ module.exports = React.createClass({
     return {url: 'http://127.0.0.1:8000/'};
   },
   getInitialState() {
-    return {contacts: null, rooms: null, audiences: null};
+    return {contacts: null};
   },
   getAllContacts() {
-    var username = Cookie.getJSON('user').username;
+    const user = Cookie.getJSON('user');
     return new Promise((resolve, reject)=>{
       $.ajax({
-        url: this.props.url + 'getallcontacts/',
+        url: this.props.url + 'contacts-list/',
         method: 'POST',
-        data: {username: username},
+        data: {username: user.username},
         success: resolve,
         error: reject
       });
     });
+  },
+  onContactClick(e, i) {
+    let contact = this.state.contacts[i];
+    this.props.set(windowTypes.CONTACT, 'c'+contact.id);
   },
   onRoomClick(e,i) {
     var r = this.state.rooms[i];
@@ -60,27 +64,36 @@ module.exports = React.createClass({
     this.setState({rooms: oldRooms, audiences: oldAudiences});
   },
   render() {
-    var rooms = [], audiences = [];
-    if (!this.state.contacts&&!this.state.rooms&&!this.state.audiences) {
-      this.getAllContacts().then((d)=>{this.setState({contacts: d.a.contacts, rooms: d.a.rooms, audiences: d.a.audiences})});
+    var rooms = [], audiences = [], contacts = [];
+    if (!this.state.contacts) {
+      this.getAllContacts().then((d)=>{this.setState({contacts: d.a})});
     } else {
-      rooms = this.state.rooms.map((r)=>({text: r.name +' | '+r.id}));
-      audiences = this.state.audiences.map((a)=>({text: a.name+' | '+a.id}));
-      if (rooms) {
-        rooms = <Menu menuItems={rooms} onItemTap={this.onRoomClick} autoWidth={false} zDepth={0}/>;
-      }
-      if (audiences) {
-        audiences = <Menu menuItems={audiences} onItemTap={this.onAudienceClick} autoWidth={false} zDepth={0}/>;
-      }
+      // rooms = this.state.rooms.map((r)=>({text: r.name +' | '+r.id}));
+      // audiences = this.state.audiences.map((a)=>({text: a.name+' | '+a.id}));
+      // if (rooms) {
+      //   rooms = <Menu menuItems={rooms} onItemTap={this.onRoomClick} autoWidth={false} zDepth={0}/>;
+      // }
+      // if (audiences) {
+      //   audiences = <Menu menuItems={audiences} onItemTap={this.onAudienceClick} autoWidth={false} zDepth={0}/>;
+      // }
+        contacts = this.state.contacts.map((c) => ({text: c.first_name+' '+c.last_name}))
+        if (contacts) {
+              contacts = <Menu menuItems={contacts} onItemTap={this.onContactClick} autoWidth={false} zDepth={0}/>;
+        }
     }
     return (
       <div className="home_tab">
         <ScrollBar>
           <FlatButton onClick={this.newAudience} style={{width: '50%'}} label={'Нова аудиторія'}/>
           <FlatButton onClick={this.newRoom} style={{float: 'right', width: '50%'}} label={'Нова кімната'}/>
+          <List subheader='Контакти' subheaderStyle={{fontSize: '1.2em'}}>
+            {contacts}
+          </List>
+          <ListDivider/>
           <List subheader='Аудиторії' subheaderStyle={{fontSize: '1.2em'}}>
             {audiences}
           </List>
+          <ListDivider/>
           <List subheader='Кімнати' subheaderStyle={{fontSize: '1.2em'}}>
             {rooms}
           </List>
