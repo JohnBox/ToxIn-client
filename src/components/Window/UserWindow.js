06aa90da1a1342e126bff5aa7d2bb77a206c6995
gott <johnboxwon@gmail.com>
2018-06-09 14:10:19 +0300

    update user edit

--- src/components/Window/UserWindow.js
+++ src/components/Window/UserWindow.js
@@ -19,11 +19,13 @@ module.exports = React.createClass({
   },
   save() {
     let user = this.state.user;
-    if (user.first_name && user.last_name && user.email) {
+    console.log(user);
+    if (user.first_name && user.last_name && user.email && user.username) {
       ajax({
         url: this.props.url + 'user-edit/',
         method: 'POST',
         data: {
+          pk: user.id,
           username: user.username,
           first_name: user.first_name,
           last_name: user.last_name,
@@ -44,6 +46,11 @@ module.exports = React.createClass({
   lastNameInput(e) {
     const user = this.state.user;
     user.last_name = e.target.value;
+    this.setState({user: user});
+  },
+  usernameInput(e) {
+    const user = this.state.user;
+    user.username = e.target.value;
     this.setState({user: user});
   },
   emailInput(e) {
@@ -91,6 +98,7 @@ module.exports = React.createClass({
         <div className="info">
           <TextField disabled={disabled} value={user.first_name} onChange={this.firstNameInput} floatingLabelText="Ім`я"/>
           <TextField disabled={disabled} value={user.last_name} onChange={this.lastNameInput} floatingLabelText="Прізвище"/>
+          <TextField disabled={disabled} value={user.username} onChange={this.usernameInput} floatingLabelText="Логін"/>
           <TextField disabled={disabled} value={user.email} onChange={this.emailInput} floatingLabelText="Електронна пошта"/>
           <TextField disabled={disabled} value={user.workplace} onChange={this.workplaceInput} floatingLabelText="Місце роботи"/>
           <TextField disabled={disabled} value={user.position} onChange={this.positionInput} floatingLabelText="Посада"/>
